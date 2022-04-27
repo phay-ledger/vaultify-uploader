f970f8a093356bcb5ec66cebb87c2696dc7f8a90
Pierre Hay <pierre.hay@ledger.fr>
2022-04-27 18:40:57 +0200

    pinata: nft metadata to ipfs

--- nft-uploader/src/index.js
+++ nft-uploader/src/index.js
@@ -5,28 +5,33 @@ import path from "path";
 
 const pinataApiKey = process.env.PINATA_API_KEY;
 const pinataSecretApiKey = process.env.PINATA_SECRET_KEY;
-
 const pinata = pinataSDK(pinataApiKey, pinataSecretApiKey);
+const pinata_ipfs_gateway = "https://gateway.pinata.cloud/ipfs/"
 
-const assets_path = "../nft-assets"
+const assets_path = "../nft-assets/images"
+const assets_metadata_path = "../nft-assets/metadata"
 
-// TODO: update this directory with actual metadata if needed.
+// TODO: update this dictionary with actual image metadata if needed.
 const nft_meta = {
-    "nft_1.png": {
-        name: "nft_1",
+    "1.png": {
+        name: "1",
         keyvalues: {
-            "nft_1": "meta"
+            "nft_1": "yoyoy 1"
         }
     },
 
-    "nft_2.png": {
-        name: "nft_2",
+    "2.png": {
+        name: "2",
         keyvalues: {
-            "nft_2": "metu"
+            "2": "vaultify 2"
         }
     },
-
 }
+
+
+// LOOP on each nft image asset
+// PIN image asset to IPFS with PINATA
+// UPDATE image in ../nft_assets/metadata/*.json with image uri
 
 fs.readdir(assets_path, function(err, files) {
 
@@ -35,10 +40,8 @@ fs.readdir(assets_path, function(err, files) {
     }
 
     files.forEach(function (file_name) {
-
         const file_path = path.join(assets_path, file_name)
         const readableStreamForFile = fs.createReadStream(file_path);
-       
         const options = {
             pinataMetadata: nft_meta[file_name],
             pinataOptions: {
@@ -47,7 +50,12 @@ fs.readdir(assets_path, function(err, files) {
         };
         pinata.pinFileToIPFS(readableStreamForFile, options).then((result) => {
             //handle results here
-            console.log(result);
+            const img_uri = path.join(pinata_ipfs_gateway, result.IpfsHash);
+            const json_metadata = file_name.substring(0, file_name.lastIndexOf("."));
+            const json_metadata_path = path.join(assets_metadata_path, json_metadata)
+            const meta = JSON.parse(fs.readFileSync(json_metadata_path, "utf8"));
+            meta["image"] = img_uri
+            fs.writeFileSync(json_metadata_path, JSON.stringify(meta, null, 2));
         }).catch((err) => {
             //handle error here
             console.log(err);
@@ -55,5 +63,29 @@ fs.readdir(assets_path, function(err, files) {
     });
 });
 
+// PIN nfts metadata to IPFS with pinata
+const options = {
+    pinataMetadata: {
+        name: 'VAULTIFY-TEST',
+        keyvalues: {
+            customKey: 'customValue',
+            customKey2: 'customValue2'
+        }
+    },
+    pinataOptions: {
+        cidVersion: 0
+    }
+};
+pinata.pinFromFS(assets_metadata_path, options).then((result) => {
+   
+
+    const baseUri = path.join(pinata_ipfs_gateway, result.IpfsHash)
+    console.log("HERE IS THE baseURI TO SET IN THE NFT CONTRACT:");
+    console.log(baseUri);
+    console.log("MADE W/ LOVE by Pi2r");
+}).catch((err) => {
+    //handle error here
+    console.log(err);
+});
 
 export default {};
